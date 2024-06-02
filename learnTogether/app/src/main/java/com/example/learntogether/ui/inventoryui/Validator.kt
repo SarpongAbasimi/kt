0b05a10092a62b00f9abf1c5958741a4218634bb
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-02 13:00:24 +0100

    Created the validator class

--- learnTogether/app/src/main/java/com/example/learntogether/ui/inventoryui/Validator.kt
+++ learnTogether/app/src/main/java/com/example/learntogether/ui/inventoryui/Validator.kt
@@ -3,11 +3,11 @@ package com.example.learntogether.ui.inventoryui
 import com.example.learntogether.model.ItemState
 
 interface Validator {
-    fun isValid(state: ItemState): Boolean
+    fun isValidEntry(state: ItemState): Boolean
 }
 
-object ValidatorImp : Validator {
-    override fun isValid(state: ItemState): Boolean {
+class ValidatorImp : Validator {
+    override fun isValidEntry(state: ItemState): Boolean {
         return with(state){
             name.isNotBlank() && price.isNotBlank() && quantity.isNotBlank()
         }
