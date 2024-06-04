9e211a7b9001736279e29052fd42fd6078227e2e
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-04 23:04:28 +0100

    Created form page and todos page

---
+++ todo/app/src/main/java/com/example/todo/ui/todos/TodoScreen.kt
@@ -0,0 +1,58 @@
+package com.example.todo.ui.todos
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.todo.R
+import com.example.todo.ui.theme.TodoTheme
+
+@Composable
+fun TodoScreen(){
+    Box(modifier = Modifier.fillMaxSize()) {
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .background(
+                MaterialTheme
+                    .colorScheme
+                    .onBackground
+            )
+        ) {
+
+        }
+
+        FloatingActionButton(
+            onClick = {},
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(bottom =
+                dimensionResource(id = R.dimen.padding_medium),
+                    end = dimensionResource(id = R.dimen.padding_medium)
+                )
+        ){
+            Icon(Icons.Filled.Add,
+                stringResource(R.string.floating_action_button),
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TodoScreenPreview(){
+    TodoTheme {
+        TodoScreen()
+    }
+}
