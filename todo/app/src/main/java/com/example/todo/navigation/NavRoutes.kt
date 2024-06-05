d74deaedfb219ed93ae3d559e09a509b5f11ef4e
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-05 18:37:22 +0100

    Added the nav interfate for the routes

---
+++ todo/app/src/main/java/com/example/todo/navigation/NavRoutes.kt
@@ -0,0 +1,18 @@
+package com.example.todo.navigation
+
+import com.example.todo.model.AddTodo
+import com.example.todo.model.Navigation
+import com.example.todo.model.Todos
+
+interface NavRoutes {
+    fun build(navigation: Navigation): String
+}
+
+class NavigationService: NavRoutes {
+   override fun build(navigation: Navigation): String {
+        return when(val nav = navigation){
+            Todos -> nav.name()
+            AddTodo -> nav.name()
+        }
+    }
+}
