c11c6ef494aae556be7998889b4533a1bbe2fb31
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-05 21:39:31 +0100

    Created Todo type

---
+++ todo/app/src/main/java/com/example/todo/model/Todo.kt
@@ -0,0 +1,16 @@
+package com.example.todo.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.text.SimpleDateFormat
+import java.util.Calendar
+
+
+@Entity
+final data class Todo(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val content: String,
+    val timestamp: String = SimpleDateFormat("yyy-MM-dd HH:mm")
+        .format(Calendar.getInstance().time)
+)
