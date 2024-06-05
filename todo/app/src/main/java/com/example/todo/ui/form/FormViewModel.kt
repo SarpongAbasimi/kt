a4c1b18bff2bb7a4d4a746cc532d3ecf982b6066
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-05 22:55:36 +0100

    Added the viewModelFactory for the form

--- todo/app/src/main/java/com/example/todo/ui/form/FormViewModel.kt
+++ todo/app/src/main/java/com/example/todo/ui/form/FormViewModel.kt
@@ -2,7 +2,10 @@ package com.example.todo.ui.form
 
 import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.todo.model.FormState
 import com.example.todo.model.Todo
 import com.example.todo.repository.TodoRepository
@@ -11,6 +14,9 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
+import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import com.example.todo.ApplicationContainer
+import com.example.todo.container.AppContainer
 
 class FormViewModel(val repository: TodoRepository, val validator: Validator): ViewModel() {
     private val _state: MutableStateFlow<FormState> = MutableStateFlow<FormState>(FormState())
@@ -26,6 +32,15 @@ class FormViewModel(val repository: TodoRepository, val validator: Validator): V
                 Log.e("TodoFormViewModel", "And error occurred whiles inserting data $e")
             }
 
+        }
+    }
+
+    companion object{
+        val Factory: ViewModelProvider.Factory = viewModelFactory {
+            initializer {
+                val container: AppContainer = (this[APPLICATION_KEY] as ApplicationContainer).container
+                FormViewModel(container.repository, container.validator)
+            }
         }
     }
 }
