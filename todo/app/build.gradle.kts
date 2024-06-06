ffbd37691f6ada6c87ae1aeeffe9cf9a3c7983aa
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-06 18:36:22 +0100

    Added room lib as dependency

--- todo/app/build.gradle.kts
+++ todo/app/build.gradle.kts
@@ -50,6 +50,7 @@ android {
 }
 
 dependencies {
+    implementation(libs.androidx.room.kts)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
