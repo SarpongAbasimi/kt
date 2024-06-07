3bc978d7bd52aaf6509a1858ff5c90a10140343b
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-07 17:11:41 +0100

    Implemented the get getAll functions to get all todos

--- todo/app/src/main/java/com/example/todo/ui/todos/TodoViewModel.kt
+++ todo/app/src/main/java/com/example/todo/ui/todos/TodoViewModel.kt
@@ -3,29 +3,31 @@ package com.example.todo.ui.todos
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.todo.ApplicationContainer
 import com.example.todo.model.Todo
-
 import com.example.todo.model.TodosState
 import com.example.todo.repository.Repository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
-class TodoViewModel(val repository: Repository): ViewModel(){
+class TodoViewModel(private val repository: Repository): ViewModel(){
 
     private val _todoState = MutableStateFlow(TodosState())
     val state = _todoState.asStateFlow()
 
     init {
-        getAllTodos()
+        viewModelScope .launch {
+            getAllTodos()
+        }
     }
 
-    private fun getAllTodos(){
-        repository.getAll().map { data: List<Todo> ->
+    suspend private fun getAllTodos(){
+        repository.getAll().collect { data: List<Todo> ->
             _todoState.update { state: TodosState -> state.copy(value = data)}
         }
     }
