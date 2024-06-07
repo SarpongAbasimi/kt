b5befd64331d6154118aea2f75460110f12e793e
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-07 21:03:22 +0100

    Added the delete icon to the view

--- todo/app/src/main/java/com/example/todo/ui/todos/TodoScreen.kt
+++ todo/app/src/main/java/com/example/todo/ui/todos/TodoScreen.kt
@@ -4,7 +4,10 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
@@ -27,9 +30,15 @@ import com.example.todo.R
 import com.example.todo.model.TodosState
 import com.example.todo.ui.theme.TodoTheme
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.todo.model.Todo
 
 @Composable
 fun TodoScreen(
@@ -37,6 +46,20 @@ fun TodoScreen(
     viewModel: TodoViewModel = viewModel(factory = TodoViewModel.Factory)
 ){
     val todoState: TodosState by viewModel.state.collectAsState()
+//    val todoState: TodosState = TodosState(
+//        listOf<Todo>(
+//            Todo(1, "I have to clean my room ", "2024-06006 19:54"),
+//            Todo(2, "I have to clean my room", "2024-06006 19:54"),
+//            Todo(3, "I have to clean my room", "2024-06006 19:54"),
+//            Todo(4, "I have to clean my room", "2024-06006 19:54"),
+//            Todo(5, "I have to clean my room", "2024-06006 19:54"),
+//            Todo(6, "I have to clean my room", "2024-06006 19:54"),
+//            Todo(7, "I have to clean my room", "2024-06006 19:54"),
+//            Todo(8, "I have to clean my room", "2024-06006 19:54"),
+//            Todo(9, "I have to clean my room", "2024-06006 19:54"),
+//            Todo(10, "I have to clean my room", "2024-06006 19:54")
+//        )
+//    )
 
     Box(modifier = Modifier.fillMaxSize()) {
         LazyColumn(modifier = Modifier
@@ -55,19 +78,43 @@ fun TodoScreen(
                     defaultElevation = 6.dp
                 ),
                     modifier = Modifier
-                        .size(width = 340.dp, height = 100.dp)
+                        .height(height = 100.dp)
+                        .fillMaxWidth()
                         .padding(
                             top = dimensionResource(id = R.dimen.padding_mini),
-                            start = dimensionResource(id = R.dimen.padding_mini)
+                            start = dimensionResource(id = R.dimen.padding_small),
+                            end = dimensionResource(id = R.dimen.padding_small)
                         )
                 ) {
-                    Column(
-                        verticalArrangement = Arrangement.Center,
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(dimensionResource(id = R.dimen.padding_mini))
-                    ) {
-                        Text(text = data.content)
+                    Box {
+                        Column(
+                            verticalArrangement = Arrangement.Center,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(dimensionResource(id = R.dimen.padding_small))
+                        ) {
+                            Text(text = data.content, fontSize = 20.sp, fontFamily = FontFamily.Serif)
+                            Row(modifier = Modifier
+                                .padding(top = 5.dp),
+                                verticalAlignment = Alignment.Bottom
+                            ) {
+                                Text(text = "CreatedAt |", fontSize = 14.sp, modifier = Modifier.padding(end = 5.dp))
+                                Text(text = data.timestamp, fontSize = 14.sp)
+                            }
+                        }
+
+                        FloatingActionButton(
+                            onClick = { /*TODO*/ },
+                            modifier = Modifier
+                            .align(Alignment.BottomEnd)
+                            .padding(5.dp)
+                            .size(35.dp),
+                            containerColor = Color.Unspecified
+                        ) {
+                            Icon(Icons.Filled.Delete,
+                                stringResource(R.string.floating_action_button_delete)
+                            )
+                        }
                     }
                 }
             }
