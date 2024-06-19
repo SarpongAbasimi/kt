271945a211d84ad1dae19a60de3d886cee4bf8de
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-19 22:13:48 +0100

    Changes to fill details page with content

--- moviedb/app/src/main/java/com/example/moviedb/ui/detail/DetailScreen.kt
+++ moviedb/app/src/main/java/com/example/moviedb/ui/detail/DetailScreen.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -16,10 +17,12 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.moviedb.ui.theme.MoviedbTheme
+import com.example.moviedb.ui.theme.PurpleGrey80
 import com.example.moviedb.util.Util
 
 @Composable
@@ -30,7 +33,9 @@ fun DetailsScreen(){
             .background(color = MaterialTheme.colorScheme.scrim)
     ) {
         PosterHandler(modifier = Modifier)
-        TitleWithData(modifier = Modifier.weight(1f))
+        TitleWithData(modifier = Modifier)
+        OverView()
+        PosterContent(modifier = Modifier.weight(1f))
     }
 }
 
@@ -41,7 +46,7 @@ fun PosterHandler(
 ){
     Card(
         shape = RoundedCornerShape(0.dp),
-        modifier = modifier.padding(bottom = 5.dp)
+        modifier = modifier.padding(bottom = 8.dp)
     ) {
         Util.ImageRequest(imageUrl)
     }
@@ -66,10 +71,43 @@ fun TitleWithData(modifier: Modifier){
                 genre ->
                 Text(
                 text = genre,
-                color = Color.White,
+                color = PurpleGrey80 ,
+                fontFamily =  FontFamily.SansSerif,
                 modifier = Modifier.padding(2.dp)
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun OverView(content: String = ""){
+    Card(colors = CardDefaults.elevatedCardColors(
+        containerColor = MaterialTheme.colorScheme.scrim
+    ), modifier = Modifier.padding(0.dp)) {
+        Text(text = "Po is gearing up to become the spiritual leader " +
+                "of his Valley of Peace, but also needs " +
+                "someone to take his place as Dragon" +
+                " Warrior. As such, he will train a " +
+                "new kung fu practitioner for " +
+                "the spot and will encounter a " +
+                "villain called the Chameleon" +
+                " who conjures villains " +
+                "from the past.",
+            textAlign = TextAlign.Justify,
+            modifier = Modifier
+                .padding(5.dp),
+            color = Color.White,
+            fontFamily =  FontFamily.SansSerif
+        )
+    }
+}
+
+@Composable
+fun PosterContent(posterPath: String = "/kDp1vUBnMpe8ak4rjgl3cLELqjU.jpg", modifier: Modifier = Modifier){
+    Row {
+        Card(modifier = modifier) {
+            Util.ImageRequest(posterPath)
         }
     }
 }
