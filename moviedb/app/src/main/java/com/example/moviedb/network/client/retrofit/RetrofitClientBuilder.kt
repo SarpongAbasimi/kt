3e26fa35fcaa51bcc5876249166306c13b7972f5
Sarpong Abasimi <sarpongabasimi@Sarpongs-MacBook-Pro.local>
2024-06-20 13:44:34 +0100

    Created an intercepter

--- moviedb/app/src/main/java/com/example/moviedb/network/client/retrofit/RetrofitClientBuilder.kt
+++ moviedb/app/src/main/java/com/example/moviedb/network/client/retrofit/RetrofitClientBuilder.kt
@@ -2,6 +2,10 @@ package com.example.moviedb.network.client.retrofit
 
 import com.example.moviedb.model.ApiKey
 import com.example.moviedb.model.BaseUrl
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
 
@@ -14,8 +18,22 @@ object RetrofitClient : RetrofitClientBuilder {
     override fun build(baseUrl: BaseUrl, apiKey: ApiKey): Retrofit {
         return Retrofit.Builder()
             .baseUrl("https://api.themoviedb.org/3/")
+            .client(okHttpClient(apiKey).build())
             .addConverterFactory(ScalarsConverterFactory.create())
             .build()
     }
+
+    fun okHttpClient(apiKey: ApiKey) = OkHttpClient().newBuilder().addInterceptor(
+        object : Interceptor {
+            override fun intercept(chain: Interceptor.Chain): Response {
+                val request: Request = chain.request()
+                    .newBuilder()
+                    .header("accept", "application/json")
+                    .header("Authorization", "${apiKey.value}")
+                    .build()
+                return chain.proceed(request)
+            }
+        }
+    )
 
 }
